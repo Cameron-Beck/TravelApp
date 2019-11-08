fcb450843d6f9f6f1ca3ae3460740aa2b1d29917
Cameron <cameron.beckit@gmail.com>
2019-11-08 22:01:08 +0000

    added delete class to country class

--- models/country.rb
+++ models/country.rb
@@ -1,33 +1,43 @@
 class Country
 
-    attr_reader :name, :visited, :id
+  attr_reader :name, :visited, :id
 
-    def initialize(options)
-      @name = options['name']
-      @visited = options['visited']
-      @id = options['id'].to_i
-    end
+  def initialize(options)
+    @name = options['name']
+    @visited = options['visited']
+    @id = options['id'].to_i
+  end
 
-    def save()
-      sql = "INSERT INTO country
-      (
-        name ,
-        visited
-      )
-      VALUES
-      (
-        $1, $2
-      )
+  def save()
+    sql = "INSERT INTO country(
+      name ,
+      visited)
+      VALUES(
+      $1, $2)
       RETURNING id"
-      values = [@name, @visited]
-      results = SqlRunner.run(sql, values)
-      @id = results.first()['id'].to_i
-    end
+    values = [@name, @visited]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
 
-    def self.delete_all()
-      sql = "DELETE FROM country"
-      SqlRunner.run( sql )
-    end
+  def self.delete_all()
+    sql = "DELETE FROM country"
+    SqlRunner.run( sql )
+  end
+
+  def delete()
+  sql = "DELETE FROM country
+  WHERE id = $1"
+  values = [@id]
+  SqlRunner.run(sql, values)
+  end
+
+  # def self.all()
+  #   sql = "SELECT * FROM students"
+  #   student_data = SqlRunner.run(sql)
+  #   students = map_items(student_data)
+  #   return students
+  # end
 
 
 
