3e3eef0dea86e9a1757dc0d5c4b15df42ce68937
Cameron <cameron.beckit@gmail.com>
2019-11-08 14:44:29 +0000

    added city table

---
+++ db/destinations.sql
@@ -0,0 +1,26 @@
+DROP TABLE bitings;
+DROP TABLE zombies;
+DROP TABLE victims;
+
+CREATE TABLE zombies
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+  type VARCHAR(255)
+);
+
+DROP TABLE city;
+DROP TABLE country;
+
+CREATE TABLE city (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  visited BOOLEAN,
+  country_id INT8 REFERENCES country(id)
+);
+
+CREATE TABLE country (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  visited BOOLEAN
+)
