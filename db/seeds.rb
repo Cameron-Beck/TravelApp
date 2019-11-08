d0484332396fa894aabc4955f8ea835cd27e3f34
Cameron <cameron.beckit@gmail.com>
2019-11-08 17:22:42 +0000

    added two countries to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 city1 = City.new({
   "name" => "London",
   "visited" => true
-})
+  })
 
 city2 = City.new({
-    "name" => "Paris",
-    "visited" => false
-})
+  "name" => "Paris",
+  "visited" => false
+  })
+
+country1 = Country.new({
+  "name" => "England",
+  "visited" => true
+  })
+
+country2 = Country.new({
+  "name" => "France",
+  "visited" => false
+  })
