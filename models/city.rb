184e8496a0a9a4cfd83eae6bcfa72e994f041f7a
Cameron <cameron.beckit@gmail.com>
2019-11-08 15:02:09 +0000

    created initialize method in City class

--- models/city.rb
+++ models/city.rb
@@ -4,8 +4,10 @@ class City
 
   attr_reader(:name, :visited, :id, :country_id)
 
-  def initalize
-    
-
+  def initalize(options)
+    @id = options['id']to_i
+    @country_id['country_id']to_i
+    @name = options['name']
+    @visited = options['visited']
   end
 end
