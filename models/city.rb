03a949188ebcb1f4a424b33521d3a392a14d53ba
Cameron <cameron.beckit@gmail.com>
2019-11-08 17:06:30 +0000

    added save function to City class

--- models/city.rb
+++ models/city.rb
@@ -10,4 +10,20 @@ class City
     @name = options['name']
     @visited = options['visited']
   end
+
+  def save()
+    sql = "INSERT INTO city
+    (
+      name ,
+      visited,
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@name, @visited]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
 end
