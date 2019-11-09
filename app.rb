7bc7873207b3a121e2f989495aa52d35c74e1d15
Cameron <cameron.beckit@gmail.com>
2019-11-09 22:13:51 +0000

    fixed bucket list countries so it only shows countries that have not been visited

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/city_controller.rb')
 require_relative('controllers/country_controller.rb')
+also_reload( '../models/*' )
 
 get '/' do
   erb( :home )
--- controllers/country_controller.rb
+++ controllers/country_controller.rb
@@ -2,7 +2,7 @@ require_relative('../models/country')
 also_reload( '../models/*' )
 
 get '/countries_visited' do
-  @country = Country.all
+  @country = Country.all_bucket_list
   erb( :countries_visited )
 end
 
